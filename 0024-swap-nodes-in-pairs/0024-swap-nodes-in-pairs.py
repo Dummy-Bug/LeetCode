fbd59057108fdeb7d516a99cc647bd5f498fe9db
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-01-26 10:44:39 +0530

    Time: 68 ms (7.97%), Space: 14 MB (15.11%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -3,24 +3,13 @@ class Solution:
         
         if not head or not head.next:
             return head;
-        curr = head; dummy = ListNode();
-        prev_link = dummy;
-
-        while curr and curr.next:
-            
-            Next = curr.next;
-            temp = Next.next;
-            Next.next = curr;
-            
-            prev_link.next = Next;
-            prev_link = curr;
-            curr = temp;
-        if not curr:
-            prev_link.next = None;
-        elif curr and not curr.next:
-            prev_link.next = curr;
-            curr.next = None
-        return dummy.next
+        
+        temp = head.next;
+        
+        head.next = self.swapPairs(head.next.next);
+        temp.next = head;
+        
+        return temp;
             
             
     
