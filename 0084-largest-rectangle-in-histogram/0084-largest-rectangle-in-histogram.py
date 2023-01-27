6445652e889318d7ca04900001d5f54d12475177
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-01-27 10:10:00 +0530

    Time: 1324 ms (38.13%), Space: 28.4 MB (62.99%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -0,0 +1,44 @@
+class Solution:
+
+	def largestRectangleArea(self, A):
+
+		nsl = []; nsr = [];
+		stack = deque([]);
+
+		for i in range(len(A)):
+
+			while stack and A[stack[-1]] >= A[i]:
+				stack.pop();
+			if not stack:
+				nsl.append(-1);
+			else:
+				nsl.append(stack[-1]);
+			stack.append(i);
+
+		stack = deque([]);
+
+		for i in range(len(A)-1,-1,-1):
+
+			while stack and A[stack[-1]] >= A[i]:
+				stack.pop();
+			if not stack:
+				nsr.append(len(A));
+			else:
+				nsr.append(stack[-1]);
+			stack.append(i);
+
+		nsr = nsr[::-1];
+		max_area = 0;print(nsl,nsr)
+        
+		for i in range(len(A)):
+
+			width = nsr[i]-nsl[i]-1;
+			curr_area = A[i]*width;
+
+			max_area = max(max_area,curr_area);
+		
+		return max_area;
+
+
+
+
