90cc56f61744002ace6f1ade2b428a1972d94d8a
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-02-02 13:12:32 +0530

    Time: 1881 ms (20.17%), Space: 30.2 MB (26.25%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,16 +1,41 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        self.k = k-1;
+        return self.helper(nums,0,len(nums)-1);
         
-        # Kth largest element is equivalent to (n-k+1)th smallest element
+    
+    def helper(self,nums,start,end):
         
-        import heapq
-        pq = []
-        
-        for num in nums:
-            heapq.heappush(pq,num) # will contain minimum element at the top
+        while start <= end:
             
-            if len(pq) > k: # if we maintain minHeap of size k 
-                heapq.heappop(pq)# then top of minHeap will contain kth
-                                 # largest number 
+            pivot_index = self.partition(nums,start,end);
+            print(pivot_index)
+            if pivot_index == self.k:
+                return nums[pivot_index];
+            
+            elif pivot_index > self.k:
+                return self.helper(nums,start,pivot_index-1);
+            else:
+                return self.helper(nums,pivot_index+1,end);
+    
+    def partition(self,nums,start,end):
         
-        return heapq.heappop(pq)
+        pivot_index = start;
+        left = start+1; right = end;
+        # conditions for kth largest and kth smallest would be different   
+        while left<=right:
+            
+            if nums[left] > nums[pivot_index]:
+                left += 1;
+            
+            elif nums[right] <= nums[pivot_index]:
+                right -= 1;
+            else:
+                nums[left],nums[right] = nums[right],nums[left];
+                left  += 1;
+                right -=1 
+        
+        nums[pivot_index],nums[left-1] = nums[left-1],nums[pivot_index];
+        
+        return left-1;
+        
