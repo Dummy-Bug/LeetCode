d02565345b74912d750ca67b9f2fd8f9cd519060
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-02-04 01:15:53 +0530

    Time: 78 ms (52.80%), Space: 14.1 MB (46.07%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def lengthOfLongestSubstring(self, string):
+        
+        freq_map = dict();
+        
+        for character in string:
+            if character not in freq_map:
+                freq_map[character] = 0;
+        
+        i = j = 0; 
+        max_length = 0;
+        
+        while j<len(string):
+            
+            
+            if freq_map[string[j]] == 0:
+                max_length = max(max_length,j-i+1);
+                freq_map[string[j]] += 1;
+                j += 1;
+                continue;
+                
+            while freq_map[string[j]] != 0:
+                freq_map[string[i]] -= 1;
+                i += 1;
+ 
+        
+        return max_length;
