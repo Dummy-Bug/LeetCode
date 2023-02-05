00f3ea5b8a8fdd772f10d2426e0d235aca5ad58d
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-02-05 10:55:59 +0530

    Added solution - LeetHub

--- Sum of length - GFG/sum-of-length.py
+++ Sum of length - GFG/sum-of-length.py
@@ -1,32 +1,15 @@
 #User function Template for python3
 
 class Solution:
-    def sumoflength(self,arr, n):
-     
-        # For maintaining distinct elements.
-        s = []
-     
-        # Initialize ending point and result
-        j = 0
-        ans = 0
-     
-        # Fix starting point
+    def sumoflength(self,arr,n):
+        s = set()
+        j = ans = 0
         for i in range(n):
-             
-            # Find ending point for current
-            # subarray with distinct elements.
-            while (j < n and (arr[j] not in s)):
-                s.append(arr[j])
+            while j < n and arr[j] not in s:
+                s.add(arr[j])
                 j += 1
-     
-            # Calculating and adding all possible
-            # length subarrays in arr[i..j]
             ans += ((j - i) * (j - i + 1)) // 2
-     
-            # Remove arr[i] as we pick new
-            # starting point from next
             s.remove(arr[i])
-     
         return ans
       
 
