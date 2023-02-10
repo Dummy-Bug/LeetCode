3c918bd5426c2dbd58bf24e0c49869f2533f4281
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-02-10 15:12:51 +0530

    Time: 1202 ms (21.32%), Space: 20.3 MB (59.39%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,24 +1,19 @@
 class Solution:
     def kClosest(self, nums: List[List[int]], k: int) -> List[List[int]]:
         
-        self.k = k-1;
-        return self.helper(nums,0,len(nums)-1);
-        
-    
-    def helper(self,nums,start,end):
+        self.k = k-1; start = 0; end = len(nums)-1;
         
         while start <= end:
-            pivot_index=random.randint(start,end);
-            pivot_index=self.partition(nums,start,end,pivot_index)
-            # pivot_index = self.partition(nums,start,end);
-            # print(pivot_index)
+            
+            pivot_index = random.randint(start,end);
+            pivot_index = self.partition(nums,start,end,pivot_index)
+
             if pivot_index == self.k:
                 return nums[:pivot_index+1];
-            
             elif pivot_index > self.k:
-                return self.helper(nums,start,pivot_index-1);
+                end = pivot_index - 1;
             else:
-                return self.helper(nums,pivot_index+1,end);
+                start = pivot_index + 1;
     
     def partition(self,points,start,end,pivot_index):
         
