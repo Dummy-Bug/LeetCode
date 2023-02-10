43730d71a3d75bfc9db17ac3a951bd5f403fee1f
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-02-10 15:11:09 +0530

    Time: 1165 ms (22.32%), Space: 20.4 MB (59.39%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,30 +1,44 @@
-import heapq;
-
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    def kClosest(self, nums: List[List[int]], k: int) -> List[List[int]]:
         
-        heap = []; k = k-1; result = [];
+        self.k = k-1;
+        return self.helper(nums,0,len(nums)-1);
         
-        for x,y in points:
-            
-            dist = (x**2+y**2)**0.5;
-            
-            if len(heap) <= k:
-                heapq.heappush(heap,[-1*dist,[x,y]]);
-                continue;
-            if -1*heap[0][0] > dist:
-                heapq.heappop(heap);
-                heapq.heappush(heap,[-1*dist,[x,y]]);
-            
-        while len(heap) != 0:
-            # print(heap)
-            dist,points = heapq.heappop(heap);
-            
-            result.append(points);
+    
+    def helper(self,nums,start,end):
         
-        return result;
+        while start <= end:
+            pivot_index=random.randint(start,end);
+            pivot_index=self.partition(nums,start,end,pivot_index)
+            # pivot_index = self.partition(nums,start,end);
+            # print(pivot_index)
+            if pivot_index == self.k:
+                return nums[:pivot_index+1];
+            
+            elif pivot_index > self.k:
+                return self.helper(nums,start,pivot_index-1);
+            else:
+                return self.helper(nums,pivot_index+1,end);
+    
+    def partition(self,points,start,end,pivot_index):
         
+        points[start],points[pivot_index]=points[pivot_index],points[start];
+        pivot = start; left = start+1;right = end;
+        
+        while left<=right:
+            
+            if points[left][0]**2+points[left][1]**2 <= points[pivot][0]**2+points[pivot][1]**2:
+                left += 1;
+            elif points[right][0]**2+points[right][1]**2 > points[pivot][0]**2+points[pivot][1]**2:
+                right -= 1;
+            else:
+                points[left],points[right] = points[right],points[left];
+                left += 1;
+                right -= 1;
+        points[left-1],points[pivot] = points[pivot],points[left-1];
+        
+        return left-1;
                 
-        
-        
+            
+            
         
