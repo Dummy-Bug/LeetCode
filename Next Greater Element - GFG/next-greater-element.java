59ed2421ea3dbac7cd0035679e25cfa4ac83b560
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2023-06-22 02:07:13 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -9,42 +9,46 @@ class GFG {
     
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
 		int t = Integer.parseInt(br.readLine().trim());
 		while(t-->0){
 		    int n = Integer.parseInt(br.readLine().trim());
 		    String inputLine[] = br.readLine().trim().split(" ");
 		    long[] arr = new long[n];
 		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
-		    long[] res = new Solution().nextLargerElement(arr, n);
+		    Solution ob = new Solution();
+		    long[] res = ob.nextLargerElement(arr, n);
 		    for (int i = 0; i < n; i++) 
-		        System.out.print(res[i] + " ");
-		    System.out.println();
+		        ot.print(res[i] + " ");
+		    ot.println();
 		}
+        ot.close();
 	}
 }
 
 
 
-
 // } Driver Code Ends
 
-class Solution
-{
-    //Function to find the next greater element for each element of the array.
-    public static long[] nextLargerElement(long[] arr, int n) {
-            long[] result = new long[n];
-            Arrays.fill(result, -1); // Initialize all elements in result array to -1
-            Stack<Integer> monoStack = new Stack<>(); // Store indices of elements in the monoStack
-    
-            for (int i = 0; i < n; i++) {
-                // Check if the current element is greater than elements at indices stored in the monoStack
-                while (!monoStack.isEmpty() && arr[i] > arr[monoStack.peek()]) {
-                    int index = monoStack.pop();
-                    result[index] = arr[i];
-                }
-                monoStack.push(i); // Push the current element index to the monoStack
+
+// User Function Template for JAVA
+
+class Solution{
+    public static long[] nextLargerElement(long[] arr, int n) { 
+        long[] result = new long[n];
+        Arrays.fill(result, -1l); // Initialize all elements in result array to -1
+        Stack<Integer> monoStack = new Stack<>(); // Store indices of elements in the monoStack
+        
+        for (int i = 0; i < n; i++) {
+            // Check if the current element is greater than elements at indices stored in the monoStack
+            while (!monoStack.isEmpty() && arr[i] > arr[monoStack.peek()]) {
+                int index = monoStack.pop();
+                result[index] = arr[i];
             }
-    
-            return result;
+            monoStack.push(i); // Push the current element index to the monoStack
         }
+        
+        return result;
+
+    } 
 }
