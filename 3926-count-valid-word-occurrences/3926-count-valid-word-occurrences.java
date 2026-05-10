d3cfc8d687d5dde12a5edb4b2b2bf563659b81c4
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-05-10 14:00:25 +0530

    Time: 66 ms (94.1%), Space: 118 MB (97.78%) - LeetHub

--- 3926-count-valid-word-occurrences/3926-count-valid-word-occurrences.java
+++ 3926-count-valid-word-occurrences/3926-count-valid-word-occurrences.java
@@ -1,68 +1,42 @@
-class Solution {
+ class Solution {
 
-    private boolean checkInvalid(String s,int index){
-        
-        char ch = s.charAt(index); 
+      private boolean isSeparator(String s, int i) {
+          char ch = s.charAt(i);
+          if (ch == ' ') return true;
+          if (ch != '-') return false;
 
-        if (ch != ' ' && ch != '-') return false;
+          int n = s.length();
+          if (i == 0 || i == n - 1) return true;
+  
+          char prev = s.charAt(i - 1);
+          char next = s.charAt(i + 1);
+          return prev == ' ' || prev == '-' || next == ' ' || next == '-';
+      }
 
-        int n = s.length();
-        if (ch == ' ') return true;
-        
-        if (index ==  n-1 && ch == '-') return true;
-        if (index == 0 && ch == '-') return true;
+      public int[] countWordOccurrences(String[] chunks, String[] queries) {
+          String s = String.join("", chunks);
+          int n = s.length();
 
-        char prevChar = s.charAt(index-1);
-        char nextChar = s.charAt(index+1);
+          Map<String, Integer> freq = new HashMap<>();
+          int start = 0;
 
-        if ((ch == '-') && (nextChar == ' ' || nextChar == '-' || prevChar == ' ' || prevChar == '-')){
-            return true;
-        }
-        return false;
-    }
-    public int[] countWordOccurrences(String[] chunks, String[] queries) {
-        
-        String joined = String.join("",chunks);
+          for (int j = 0; j < n; j++) {
+              if (isSeparator(s, j)) {
+                  if (start < j) {
+                      String word = s.substring(start, j);
+                      freq.put(word, freq.getOrDefault(word, 0) + 1);
+                  }
+                  start = j + 1;
+              } else if (j == n - 1) {
+                  String word = s.substring(start, j + 1);
+                  freq.put(word, freq.getOrDefault(word, 0) + 1);
+              }
+          }
 
-        int i = 0; int j = 0;
-        int n = joined.length();
-
-        List<String> al = new ArrayList<>();
-
-        while(j < n){
-
-            boolean isInvalid = checkInvalid(joined,j);
-
-            if (isInvalid){
-                if (i < j){
-                    al.add(joined.substring(i,j));
-                }
-                j++;
-                i = j;
-            }
-            
-            else {
-                if (j == n-1){
-                    al.add(joined.substring(i,j+1));
-                }
-                j++;
-            }
-        }
-        
-        Map<String,Integer> map = new HashMap<>();
-
-        for (int x = 0; x < al.size(); x++){
-            String word = al.get(x);
-            map.put(word,map.getOrDefault(word,0)+1);
-        }
-
-        int m = queries.length;
-        int[] ans = new int[m];
-
-        for (int y = 0; y < m; y++){
-            String word = queries[y];
-            ans[y] = map.getOrDefault(word,0);
-        }
-        return ans;
-    }
-}
+          int[] ans = new int[queries.length];
+          for (int i = 0; i < queries.length; i++) {
+              ans[i] = freq.getOrDefault(queries[i], 0);
+          }
+          return ans;
+      }
+  }
