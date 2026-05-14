3df043816d921fb41b7cc56b185dd6ca7c682c57
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-05-14 11:31:57 +0530

    Time: 4 ms (74.8%), Space: 139.1 MB (93.18%) - LeetHub

---
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int smallestBalancedIndex(int[] nums) {
+
+        long sum = 0;
+        int n = nums.length;
+
+        for (int num : nums){
+            sum += num;
+        }
+
+        long product = 1;
+        int smallestIndex = n;
+
+        for (int i = n-1; i >= 0; i--){
+
+            sum -= nums[i];
+            if (sum == product){
+                smallestIndex = i;
+            }
+            if ((sum < product) || (product > sum/nums[i])){
+                break;
+            }
+            
+            product = product * nums[i];
+        }
+
+        if (smallestIndex == n){
+            return -1;
+        }
+        return smallestIndex;
+        
+    }
+}
