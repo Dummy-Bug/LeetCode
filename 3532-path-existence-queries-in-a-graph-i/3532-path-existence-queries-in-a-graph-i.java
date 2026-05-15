13c07c4894f1b0341df675e0d66570c2aad39202
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-05-15 15:55:37 +0530

    Time: 3 ms (98.1%), Space: 163.2 MB (65.71%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -1,45 +1,21 @@
 class Solution {
-    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+      public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff,int[][] queries) {
+          
+          int[] comp = new int[n];
 
-        int[] components = new int[n];
-        Arrays.fill(components,-1);
-        components[n-1] = n - 1;
+          for (int i = 1; i < n; i++) {
+              if (nums[i] - nums[i - 1] <= maxDiff) {
+                  comp[i] = comp[i - 1];
+              } else {
+                  comp[i] = i;
+              }
+          }
 
-        for (int i = 0; i < n-1; i++){
-
-            int num = nums[i];
-            int next = nums[i+1];
-
-            int diff = nums[i + 1] - nums[i];
-
-            if (diff <= maxDiff){
-
-                if (components[i] != -1){
-                    components[i + 1] = components[i];
-                }else{
-                    components[i] = i;
-                    components[i+1] = i;
-                }
-            }
-            if (components[i] == -1){
-                components[i] = i;
-            }
-        }
-
-        boolean [] ans = new boolean[queries.length];
-
-        for (int i = 0; i < queries.length; i++){
-            
-            int [] q = queries[i];
-            
-            int u = q[0];
-            int v = q[1];
-
-            if (components[u] == components[v]){
-                ans[i] = true;
-            }
-        }
-        return ans;
-        
-    }
-}
+          boolean[] ans = new boolean[queries.length];
+          for (int i = 0; i < queries.length; i++) {
+              int u = queries[i][0], v = queries[i][1];
+              ans[i] = comp[u] == comp[v];
+          }
+          return ans;
+      }
+  }
