1f9cb34a20812bf2f5a2b60293e56b83aff585b6
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-05-23 06:47:59 +0530

    Time: 3 ms (70.82%), Space: 138.4 MB (76.72%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.java
+++ 3840-house-robber-v/3840-house-robber-v.java
@@ -1,26 +1,22 @@
 class Solution {
     public long rob(int[] nums, int[] colors) {
-
+        
         int n = nums.length;
-        long [] dp = new long[n];
-
-        long first = nums[0];
-        long second = 0;
-
-        for (int i = 1; i < n; i++){
-            long skip = first;
-            long take = 0l;
-
-            if (colors[i] != colors[i-1]){
-                take = nums[i] + first;
-            }else{
-                take = nums[i] + second;
-            }
-            second = first;
-            first = Math.max(take,skip);
+        if (n == 1){
+            return (long)nums[0];
         }
+        long[] dp = new long[n];
+        dp[0] = (long)nums[0];
+        dp[1] = colors[0] != colors[1] ? (long)nums[0] + nums[1] : Math.max(nums[0],nums[1]);
 
-        return first;
-
+        for (int i = 2; i < n; i++){
+            if (colors[i] == colors[i-1]){
+                dp[i] = Math.max(dp[i-1],nums[i] + dp[i-2]);
+            }
+            else{
+                dp[i] = nums[i] + dp[i-1];
+            }
+        }
+        return dp[n-1];
     }
 }
