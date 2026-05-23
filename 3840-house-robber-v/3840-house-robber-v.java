65902c029cdc47cc2c35bda73a3cfb6fff783ca2
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-05-23 06:52:45 +0530

    Time: 389 ms (5.25%), Space: 153.6 MB (45.25%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.java
+++ 3840-house-robber-v/3840-house-robber-v.java
@@ -5,18 +5,23 @@ class Solution {
         if (n == 1){
             return (long)nums[0];
         }
-        long[] dp = new long[n];
-        dp[0] = (long)nums[0];
-        dp[1] = colors[0] != colors[1] ? (long)nums[0] + nums[1] : Math.max(nums[0],nums[1]);
+
+        long secondPrev = (long)nums[0];
+        long firstPrev = colors[0] != colors[1] ? (long)nums[0] + nums[1] : Math.max(nums[0],nums[1]);
 
         for (int i = 2; i < n; i++){
+            
+            long current = 0L;
+            
             if (colors[i] == colors[i-1]){
-                dp[i] = Math.max(dp[i-1],nums[i] + dp[i-2]);
+                current = Math.max(firstPrev,nums[i] + secondPrev);
             }
             else{
-                dp[i] = nums[i] + dp[i-1];
+                current = nums[i] + firstPrev;
             }
+            secondPrev = firstPrev;
+            firstPrev = current;
         }
-        return dp[n-1];
+        return firstPrev;
     }
 }
