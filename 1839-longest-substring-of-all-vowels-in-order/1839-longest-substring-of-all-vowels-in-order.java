bd0e2f1313b2e48a70f108f09a1b6347e01dec6d
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-05-30 10:47:58 +0530

    Time: 20 ms (74.5%), Space: 50.8 MB (58.05%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
@@ -1,47 +1,25 @@
 class Solution {
-
-    private boolean isVowel(char ch) {
-
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-            return true;
-        }
-        return false;
-    }
-
     public int longestBeautifulSubstring(String word) {
 
-        int maxLength = 0;
-        int vowelCount = 0;
-        Set<Character> vowelSet = new HashSet<>();
+        int n = word.length(), maxLength = 0, runLen = 1, distinct = 1;
 
-        int i = 0, j = 0, n = word.length();
+        for (int j = 1; j < n; j++) {
 
-        while (j < n) {
+            char prev = word.charAt(j - 1);
+            char cur = word.charAt(j);
 
-            int ch = word.charAt(j) - 'a';
-
-            if (isVowel(word.charAt(j))) {
-                vowelSet.add(word.charAt(j));
-            }
-
-            if (j <= 0 || word.charAt(j - 1) - 'a' > ch) {
-                
-                vowelSet.clear();
-                i = j;
-
-                if (isVowel(word.charAt(j))) {
-                    vowelSet.add(word.charAt(j));
+            if (cur < prev) {
+                runLen = 1;
+                distinct = 1;
+            } else {
+                runLen++;
+                if (cur > prev) {
+                    distinct++;
                 }
-            } 
-            else {
-
-                if (word.charAt(j - 1) - 'a' <= ch) {
-                    if (vowelSet.size() == 5) {
-                        maxLength = Math.max(maxLength, j - i + 1);
-                    }
-                }
+                ;
             }
-            j++;
+            if (distinct == 5)
+                maxLength = Math.max(maxLength, runLen);
         }
         return maxLength;
     }
