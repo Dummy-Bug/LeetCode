8bf3f7b30f6db22a15ea5a135f3bd0d07f84edcc
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-02 14:27:13 +0530

    Time: 57 ms (49.15%), Space: 47.7 MB (33.9%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -1,37 +1,89 @@
 class Solution {
-    public int numTriplets(int[] nums1, int[] nums2) {
+      
+      List<Integer> primes = new ArrayList<>();
 
-        int count = helper(nums1,nums2) + helper(nums2,nums1);
+      private void populatePrimes(){
+          
+          boolean[] prime = new boolean[100002];
+          Arrays.fill(prime, true);
+          prime[0] = prime[1] = false;
 
-        return count;
-    }
+          for (int i = 2; i < 100001; i++){
 
-    private int helper(int[] nums1, int[] nums2){
+              if (prime[i]){
+                  
+                  primes.add(i);
+                  
+                  for (long m = (long) i * i; m < 100002; m += i) {
+                    prime[(int) m] = false;
+                  }
+              }
+          }
+      }
 
-        Map<Long,Integer> map = new HashMap<>();
+      public int numTriplets(int[] nums1, int[] nums2){
+          populatePrimes();
+          return helper(nums1, nums2) + helper(nums2, nums1);
+      }
 
-        int m = nums1.length;
-        int n = nums2.length;
+      private int helper(int[] nums1, int[] nums2){
+          Map<Integer,Integer> freq = new HashMap<>();
+          for (int x : nums2){
+              freq.put(x, freq.getOrDefault(x, 0) + 1);
+          }
+          long count = 0;
+          for (int num : nums1){
+              count += countPairs(num, freq);
+          }
+          return (int) count;
+      }
 
-        for (int i = 0; i < n; i++){
+      private long countPairs(int num, Map<Integer,Integer> freq){
 
-            for (int j = i + 1; j < n; j++){
+          long S = (long) num * num;
 
-                long product = nums2[i] * (long)nums2[j];
+          Map<Integer,Integer> f = new HashMap<>();
+          int x = num;
+          for (int p : primes){
+              if ((long) p * p > x) break;
+              while (x % p == 0){
+                  f.put(p, f.getOrDefault(p, 0) + 1);
+                  x /= p;
+              }
+          }
+          if (x > 1){
+              f.put(x, f.getOrDefault(x, 0) + 1);
+          }
 
-                map.put(product , map.getOrDefault(product , 0) + 1);
-            }
-        }
+          // double exponents → enumerate all divisors of S
+          List<Long> divisors = new ArrayList<>();
+          divisors.add(1L);
+          for (Map.Entry<Integer,Integer> e : f.entrySet()){
+              int p = e.getKey();
+              int exp = e.getValue() * 2;
+              int sz = divisors.size();
+              long pw = 1;
+              for (int t = 1; t <= exp; t++){
+                  pw *= p;
+                  for (int k = 0; k < sz; k++){
+                      divisors.add(divisors.get(k) * pw);
+                  }
+              }
+          }
 
-        int count = 0;
-
-        for (int num : nums1){
-
-            long square = (long)num*num;
-            count += map.getOrDefault(square , 0);
-        }
-
-        return count;
-
-    }
-}
+          // pair each divisor d with S/d, count
+          long count = 0;
+          for (long d : divisors){
+              long other = S / d;
+              if (d > other) continue;
+              if (other > 100000) continue;
+              int fd = freq.getOrDefault((int) d, 0);
+              if (d == other){
+                  count += (long) fd * (fd - 1) / 2;
+              } else {
+                  count += (long) fd * freq.getOrDefault((int) other, 0);
+              }
+          }
+          return count;
+      }
+  }
