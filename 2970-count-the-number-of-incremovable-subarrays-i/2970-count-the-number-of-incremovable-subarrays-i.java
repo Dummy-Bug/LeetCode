d4b0453a03a030b2cd5cbc8cd3ca761f238a71dc
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-03 07:17:42 +0530

    Time: 0 ms (100%), Space: 44.8 MB (62.5%) - LeetHub

--- 2970-count-the-number-of-incremovable-subarrays-i/2970-count-the-number-of-incremovable-subarrays-i.java
+++ 2970-count-the-number-of-incremovable-subarrays-i/2970-count-the-number-of-incremovable-subarrays-i.java
@@ -1,20 +1,48 @@
 class Solution {
-      public int incremovableSubarrayCount(int[] nums) {
+    public int incremovableSubarrayCount(int[] nums) {
+        
+        int n = nums.length;
+        int left = 0;
 
-          int n = nums.length, count = 0;
-          for (int i = 0; i < n; i++)
-              for (int j = i; j < n; j++)
-                  if (increasingAfterRemoval(nums, i, j)) count++;
-          return count;
-      }
+        for (int i = 1; i < n; i++){
+            if (nums[i - 1] >= nums[i]){
+                break;
+            }
+            left = i;
+        }
 
-      private boolean increasingAfterRemoval(int[] nums, int l, int r) {
-          int prev = Integer.MIN_VALUE;
-          for (int k = 0; k < nums.length; k++) {
-              if (k >= l && k <= r) continue;       
-              if (nums[k] <= prev) return false;   
-              prev = nums[k];
-          }
-          return true;
-      }
-  }
+        if (left == n - 1){
+            return (n*(n+1))/2;
+        }
+
+        int right = n - 1;
+        for (int i = n - 2; i >= 0; i--){
+            if (nums[i] >= nums[i + 1]){
+                break;
+            }
+            right = i;
+        }
+
+        int count = 0;
+        for (int i = left + 1; i >= 0; i--){
+
+            if ( i!= 0 && nums[i - 1] >= nums[right]){
+
+                int j = right;
+                while ( j < n && nums[i - 1] >= nums[j] ){
+                    j++;
+                }
+                if (j == n){
+                    count = count + 1;
+                }
+                else {
+                    count += n - j + 1;
+                }
+            }
+            else {
+                count += n - right + 1;
+            }
+        }
+        return count;
+    }
+}
