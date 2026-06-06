060635df608ca558fd39940c64275a5d8c66248b
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-06 16:11:09 +0530

    Time: 10 ms (83.71%), Space: 46.3 MB (84.06%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,30 @@
+class Solution {
+  
+      public int calculate(String s) {
+        
+          Deque<Integer> stack = new ArrayDeque<>();
+          int result = 0, sign = 1, num = 0;
+
+          for (int i = 0; i < s.length(); i++) {
+              char c = s.charAt(i);                      
+
+              if (Character.isDigit(c)) {
+                  num = num * 10 + (c - '0');
+              } else if (c == '+' || c == '-') {
+                  result += sign * num;
+                  num = 0;
+                  sign = (c == '+') ? 1 : -1;
+              } else if (c == '(') {
+                  stack.push(result);
+                  stack.push(sign);
+                  result = 0;
+                  sign = 1;
+              } else if (c == ')') {
+                  result += sign * num;
+                  num = 0;
+                  result = stack.pop() * result + stack.pop();
+              }
+          }
+          return result + sign * num;
+      }
+  }
