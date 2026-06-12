32c4dc522755a975878a0787ad9edf9e9a5115e0
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-12 07:06:48 +0530

    Time: 64 ms (53.15%), Space: 152 MB (81.08%) - LeetHub

--- 3613-minimize-maximum-component-cost/3613-minimize-maximum-component-cost.java
+++ 3613-minimize-maximum-component-cost/3613-minimize-maximum-component-cost.java
@@ -1,82 +1,50 @@
 class Solution {
+  
+      class DSU {
+          int[] parent, size;
 
-    class DSU {
-        int parent[];
-        int size[];
+          DSU(int n) {
+              parent = new int[n];
+              size = new int[n];
+              for (int i = 0; i < n; i++) {
+                  parent[i] = i;
+                  size[i] = 1;
+              }
+          }
 
-        DSU(int n){
-            parent = new int[n];
-            size = new int[n];
-            build(n);
-        }
+          int find(int x) {
+              if (parent[x] == x) return x;
+              parent[x] = find(parent[x]);
+              return parent[x];
+          }
+  
+          boolean union(int x, int y) {
+              int px = find(x), py = find(y);
+              if (px == py) return false;
 
-        private void build(int n){
+              if (size[px] < size[py]) {
+                  parent[px] = py;
+                  size[py] += size[px];
+              } else {
+                  parent[py] = px;
+                  size[px] += size[py];
+              }
+              return true;
+          }
+      }
 
-            for (int i = 0; i < n; i++){
-                parent[i] = i;
-                size[i] = 1;
-            }
-        }
+      public int minCost(int n, int[][] edges, int k) {
+          Arrays.sort(edges, (a, b) -> Integer.compare(a[2], b[2]));
 
-        public int getParent(int node){
+          DSU dsu = new DSU(n);
+          int needed = n - k;
+          int merges = 0;
 
-            if (parent[node] == node){
-                return node;
-            }
-            return parent[node] = getParent(parent[node]);
-        }
-
-        public int getSize(int node){
-            return size[node];
-        }
-
-        public void union(int x, int y){
-
-            int px = getParent(x);
-            int py = getParent(y);
-
-            if (px == py)return;
-
-            int sx = getSize(px);
-            int sy = getSize(py);
-
-            if (sx >= sy){
-                sx += sy;
-                parent[py] = px;
-            }
-            else{
-                sy += sx;
-                parent[px] = py;
-            }
-        }
-
-        public boolean isCycle(int x , int y){
-            return getParent(x) == getParent(y);
-        }
-
-    }
-
-    public int minCost(int n, int[][] edges, int k) {
-        
-        Arrays.sort(edges , (a,b) -> Integer.compare(a[2] , b[2]));
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(b[2] , a[2]));
-
-        DSU dsu = new DSU(n);
-
-        for (int [] edge : edges){
-
-            if (!dsu.isCycle(edge[0] , edge[1])){
-                dsu.union(edge[0] , edge[1]);
-                pq.offer(new int []{ edge[0] , edge[1] , edge[2] });
-            }
-        }
-        while (k-- != 1 && !pq.isEmpty()){
-            pq.poll();
-        }
-        if (!pq.isEmpty()){
-            return pq.poll()[2];
-        }
-        return 0;
-    }
-}
+          for (int[] e : edges) {
+              if (dsu.union(e[0], e[1])) {
+                  if (++merges == needed) return e[2];
+              }
+          }
+          return 0;
+      }
+  }
