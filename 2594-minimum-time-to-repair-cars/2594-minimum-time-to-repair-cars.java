08fe1459d8c3766a4b36040cb29d6f05975f8f91
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-15 11:10:37 +0530

    Time: 18 ms (84.35%), Space: 58.5 MB (29.25%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.java
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.java
@@ -1,34 +1,38 @@
 class Solution {
 
-    private boolean helper(long budget , int[] ranks, long cars){
+      private boolean helper(long budget, int[] ranks, long cars) {
 
-        long repaired = 0;
+          long repaired = 0;
+  
+          for (int r : ranks) {
+              long x = budget / r;                
+              long k = (long) Math.sqrt((double) x);   
+              
+              while ((k + 1) * (k + 1) <= x) k++;  
+              while (k * k > x) k--;             
 
-        for (int r : ranks){
-            repaired += (long)Math.sqrt(budget/r);
-            if (repaired >= cars) return true;
-        }
-        return repaired >= cars;
-        
-    }
+              repaired += k;
+              if (repaired >= cars) return true;   
+          }
+          return repaired >= cars;
+      }
 
-    public long repairCars(int[] ranks, int cars) {
+      public long repairCars(int[] ranks, int cars) {
+  
+          long low = 1L;
+          long high = (long) cars * cars * 100;   
+          long ans = high;
 
-        long low = 1L;
-        long high = (long)cars*cars*100;
+          while (low <= high) {
+              long mid = low + ((high - low) >> 1);
 
-        long ans = high;
-
-        while(low <= high){
-
-            long mid = low + ((high-low)>>1) ;
-
-            if (helper(mid,ranks,cars)){
-                ans = mid;
-                high = mid - 1;
-            }
-            else low = mid + 1;
-        }
-        return ans;
-    }
-}
+              if (helper(mid, ranks, cars)) {
+                  ans = mid;
+                  high = mid - 1;
+              } else {
+                  low = mid + 1;
+              }
+          }
+          return ans;
+      }
+  }
