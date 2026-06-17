09adf14891b550ac13be670ab428bfe9eda3566d
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-17 10:51:44 +0530

    Time: 1 ms (59.65%), Space: 42.7 MB (17.03%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,27 +1,20 @@
 class Solution {
     public int reverseBits(int n) {
-        
-        final int BASE = 2;
+      
+      int i = 0, j = 31, num = 0;
+      
+      while (i < j) {
+          
+          int b1 = (n >> i) & 1;
+          int b2 = (n >>> j) & 1;
+          
+          num += b1 * (1 << j);
+          num += b2 * (1 << i);
+          
+          i++;
+          j--;
+      }
+      return num;
+  }
 
-        int i = 0;
-        int j = 31;
-
-        int num = 0;
-
-        while (i < j){
-
-            int b1 = (n>>i)&1;
-            int b2 = (n>>>j)&1;
-
-            num += b1*(int)Math.pow(BASE,j);
-            num += b2*(int)Math.pow(BASE,i);
-
-            i++;
-            j--;
-
-        }
-
-        return num;
-
-    }
 }
