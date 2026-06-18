0dea4a3c83934c2b4b23d6519c8051af6d7521bc
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-18 15:23:17 +0530

    Time: 5 ms (62.8%), Space: 53.8 MB (23.57%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,21 +1,8 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-
-        if (n == 0) {
-            List<Integer> base = new ArrayList<>();
-            base.add(0);
-            return base;
-        }
-
-        List<Integer> prev = grayCode(n - 1);
-        List<Integer> curr = new ArrayList<>(prev); 
-
-        int placeValue = 1 << (n - 1);
-
-        for (int i = prev.size() - 1; i >= 0; i--) {
-            curr.add(placeValue + prev.get(i));
-        }
-
-        return curr;
-    }
+      List<Integer> result = new ArrayList<>();
+      for (int i = 0; i < (1 << n); i++)
+          result.add(i ^ (i >> 1));
+      return result;
+  }
 }
