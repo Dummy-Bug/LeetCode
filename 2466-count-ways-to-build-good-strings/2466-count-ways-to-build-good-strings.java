216c5571c9671e6b69efb1eb143b8fc1159aebe4
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-25 10:57:36 +0530

    Time: 13 ms (28.41%), Space: 56.6 MB (30.63%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -1,36 +1,33 @@
 class Solution {
 
-    private long [] dp;
+    private long[] dp;
     private final int MOD = 1000000007;
 
     public int countGoodStrings(int low, int high, int zero, int one) {
-        
+
         dp = new long[high + 1];
-
-        Arrays.fill(dp, -1); 
-        long lowCount = helper(low - 1, zero, one);
-
         Arrays.fill(dp, -1);
-        long highCount = helper(high, zero , one);
 
-        return (int)(((highCount - lowCount) % MOD + MOD) % MOD);
+        long highCount = helper(high, zero, one);
+        long lowCount  = helper(low - 1, zero, one);
+
+        return (int) (((highCount - lowCount) % MOD + MOD) % MOD);
     }
 
-    private long helper(int length , int zero , int one){
+    private long helper(int length, int zero, int one) {
 
         if (length == 0) return 0;
-
         if (dp[length] != -1) return dp[length];
 
-        long zeroCount = 0l , oneCount = 0l;
+        long zeroCount = 0L, oneCount = 0L;
 
-        if (length - zero >= 0){
-            zeroCount = (1 + helper(length - zero, zero, one)%MOD);
+        if (length - zero >= 0) {
+            zeroCount = 1 + helper(length - zero, zero, one);
         }
-        if (length - one >= 0){
-            oneCount = (1 + helper(length - one, zero , one%MOD));
+        if (length - one >= 0) {
+            oneCount = 1 + helper(length - one, zero, one);
         }
-        return dp[length] = (zeroCount + oneCount)%MOD;
 
+        return dp[length] = (zeroCount + oneCount) % MOD;
     }
 }
