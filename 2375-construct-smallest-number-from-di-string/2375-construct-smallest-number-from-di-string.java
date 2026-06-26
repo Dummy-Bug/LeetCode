aaeaab48cd50783e397f222f041a789512f54bef
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-06-26 07:04:26 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.47%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
@@ -1,41 +1,23 @@
 class Solution {
-    private void reverse(int [] nums, int start , int end){
-
-        while (start < end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-
-            start++;
-            end--;
-        }
-    }
     public String smallestNumber(String pattern) {
-        
         int n = pattern.length();
-        int [] ans = new int[n + 1];
-        
-        for (int i = 0; i <= n ; i++) ans[i] = i + 1;
+        char[] res = new char[n + 1];
+        for (int i = 0; i <= n; i++) res[i] = (char) ('1' + i);
 
-        int prevD = -1;
-
-        for (int i = 0; i < n; i++){
-
-            char ch = pattern.charAt(i);
-
-            if (ch == 'D' && prevD == -1) prevD = i ;
-
-            else if (ch == 'I' && prevD != -1){
-                reverse(ans,prevD,i);
-                prevD = -1;
-            }
+        int i = 0;
+        while (i < n) {
+            int j = i;
+            while (j < n && pattern.charAt(j) == 'D') j++;
+            reverse(res, i, j);
+            i = Math.max(j, i + 1);
         }
+        return new String(res);
+    }
 
-        if (prevD != -1) reverse(ans,prevD,n);
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int num : ans) sb.append(num);
-        return sb.toString();
+    private void reverse(char[] a, int lo, int hi) {
+        while (lo < hi) {
+            char t = a[lo]; a[lo] = a[hi]; a[hi] = t;
+            lo++; hi--;
+        }
     }
 }
