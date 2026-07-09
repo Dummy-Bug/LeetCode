3811f088e52e5c0fc739aa0e20b0f60e85f03e57
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-07-09 07:23:32 +0530

    Time: 10 ms (21.88%), Space: 48.8 MB (92.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,39 +1,40 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        List<int[]> stack = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
+        Arrays.sort( intervals,(a,b) -> {
+            if (a[0] != b[0]){
+                return Integer.compare(a[0] , b[0]);
+            }
+            else return Integer.compare(a[1] ,b[1]);
+            } );
 
-        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
-        stack.add(new int[]{intervals[0][0],intervals[0][1]});
-
+        ans.add(intervals[0]);
         int n = intervals.length;
+
         for (int i = 1; i < n; i++){
 
-            int sz = stack.size();
-            int[] past = stack.get(sz - 1);
+            int[] prev = ans.get(ans.size() - 1);
 
-            if (intervals[i][0] <= past[1]){
-                stack.remove(sz - 1);
-                int end = Math.max(past[1] , intervals[i][1]);
-                stack.add(new int[]{past[0] , end});
+            if (prev[1] >= intervals[i][0]){
+                ans.remove(ans.size() - 1);
+                int end = Math.max(prev[1] , intervals[i][1]);
+                ans.add(new int[]{prev[0] , end});
             }
             else {
-                stack.add(intervals[i]);
+                ans.add(intervals[i]);
             }
         }
+        int size = ans.size();
 
-        int sz = stack.size();
-        int [][] ans = new int[sz][2];
+        int [][] output = new int[size][2];
 
-        for (int i = 0; i < sz; i++){
+        for (int i = 0; i < size;i++){
+            int start = ans.get(i)[0];
+            int end = ans.get(i)[1];
 
-            int start = stack.get(i)[0];
-            int end = stack.get(i)[1];
-
-            ans[i][0] = start;
-            ans[i][1] = end;
+            output[i] = new int[]{start , end};
         }
-        
-        return ans;
+        return output;
     }
 }
