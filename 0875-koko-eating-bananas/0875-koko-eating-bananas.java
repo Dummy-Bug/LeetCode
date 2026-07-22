02f0d67148286071c8ec563abfebeae5a0caae81
LAxY <36243366+Dummy-Bug@users.noreply.github.com>
2026-07-22 08:56:59 +0530

    Time: 8 ms (68.45%), Space: 47.7 MB (74.63%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,39 +1,29 @@
-class Solution {
+class Solution { 
+    private boolean canEat(int[] piles, int h, long rate) {
+        long time = 0; 
+        for (int i = 0; i < piles.length; i++) {
+            time += ((long)piles[i] + rate - 1) / rate; 
+            if (time > h) return false; 
+        } 
+        return true; 
+    } 
 
-    private boolean canEat(int [] piles , int h , int rate){
-
-        int time = 0;
-
-        for (int i = 0; i < piles.length; i++){
-
-            time += (piles[i] + rate - 1)/rate ;
-            if (time > h) return false;
-        }
-        return true;
-    }
-    public int minEatingSpeed(int[] piles, int h) {
-        
-        long low = 1;
-        long high = 1;
-
-        for (int num : piles){
-            high = Math.max(high , (long)num);
-        }
-
-        long ans = 1;
-
-        while(low <= high){
-
-            long mid = (low + high)>>1;
-
-            if (canEat(piles,h,(int)mid)){
-                ans = mid;
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
-        }
-        return (int)ans;
-    }
+    public int minEatingSpeed(int[] piles, int h) { 
+        long low = 1; 
+        long high = 1; 
+        for (int num : piles) { 
+            high = Math.max(high, (long)num); 
+        } 
+        long ans = 1; 
+        while (low <= high) { 
+            long mid = (low + high) >> 1; 
+            if (canEat(piles, h, mid)) { 
+                ans = mid; 
+                high = mid - 1; 
+            } else { 
+                low = mid + 1; 
+            } 
+        } 
+        return (int)ans; 
+    } 
 }
